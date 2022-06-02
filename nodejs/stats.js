29d482c3fe904029d35388b1f925f331711e33c8
caiquemiranda <caiquemirandas@gmail.com>
2022-06-02 06:19:36 -0300

    start-studies

---
+++ nodejs/stats.js
@@ -0,0 +1,15 @@
+const os = require('os')
+
+const { freeMem, totalMem } = os
+
+const total = parseInt(totalMem() / 1024 / 1024)
+const mem = parseInt(freeMem() / 1024 / 1024)
+const percent = parseInt((mem / total) * 100)
+
+const stats = {
+    free: '${mem} MB',
+    total: '${total} MB',
+    percent: '${percent}%'
+}
+
+console.table(stats);
